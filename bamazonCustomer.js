26ad601a55a1836aaf166f921b02b58e5fea8c21
Micaela Bray <micaela.bray@gmail.com>
2019-07-22 20:47:01 -0500

    updates connection path

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -10,7 +10,7 @@ var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "Mickey08!",
+    password: "", //Input your password here
     database: "bamazon_db"
 });
 
